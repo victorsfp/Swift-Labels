3ada6046566adcb03c301f1268c4ccb2122ae326
Victor Feitosa <victor.s.feitosa2@hotmail.com>
2021-12-13 11:52:50 -0800

    projeto

---
+++ Labels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Labels
+//
+//  Created by user210273 on 12/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
